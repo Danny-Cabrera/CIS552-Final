0038764e2932f58fb7068409806f27bb60cb57af
danny-cabrera <danielc00@live.com>
2014-04-20 23:13:22 -0400

    trying to get Arrays

--- Align.hs
+++ Align.hs
@@ -10,6 +10,9 @@ import Bio.Sequence
 import Bio.Alignment.SAlign
 import TestData
 import Test.HUnit
+import Main
+import Data.Array
+
 
 readLength :: Int
 readLength = 5
@@ -18,6 +21,7 @@ readLength = 5
 --Make sequence data of length multiple of readLength
 padSeq :: SeqData -> SeqData
 padSeq s = let q = lengthSeq s `mod` readLength in
+			   if q == 0 then s else
                s `appendSeq` (buildSeq $ readLength - q)  
 
 appendSeq :: SeqData -> SeqData -> SeqData
@@ -30,8 +34,12 @@ buildSeq n = fromStr "T" `appendSeq` (buildSeq $ n - 1)
 lengthSeq :: SeqData -> Int
 lengthSeq s = length (toStr s)
 
+indexer :: SeqData -> [(SeqData, Int)]
+indexer s = indexGenome (padSeq s) [] 0
+
 indexGenome :: SeqData -> [(SeqData, Int)] -> Int -> [(SeqData, Int)] 
-indexGenome seq l index = indexGenome (dropSeq readLength seq) ((takeSeq readLength seq, index): l) (index + 1) 
+indexGenome seq l index = if lengthSeq seq == 0 then l else
+							indexGenome (dropSeq readLength seq) ((takeSeq readLength seq, index): l) (index + 1) 
 
 
 dropSeq :: Int -> SeqData -> SeqData
@@ -42,13 +50,11 @@ takeSeq i s = fromStr $ take i (toStr s)
 
 
 test1 :: Test
-test1 =  indexGenome refSeqShort [] 0 ~?= [(fromStr "TTGGC", 2), (fromStr "TCAAG", 1), (fromStr "ACTGG", 0)]
+test1 =  indexer refSeqShort ~?= [(fromStr "TTGGC", 2), (fromStr "TCAAG", 1), (fromStr "ACTGG", 0)]
 
 splitRef :: [(SeqData, Int)]
 splitRef = indexGenome refSeq [] 0
 
 --Needleman Wunsch Algorithm for scoring
-
---global_score :: (Num a, Ord a) => SubstMx t a -> (a, a) -> Sequence t -> Sequence t -> a
---global_score matrix  seq1
+show align seq1 refSeqShort
 
