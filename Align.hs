07283232c3226a7a69b350d5b325575883673693
danny-cabrera <danielc00@live.com>
2014-04-27 18:41:47 -0400

    Sorry I keep making you upset, I'll keep trying to be better

--- Align.hs
+++ Align.hs
@@ -11,7 +11,7 @@ import Test.HUnit
 import NeedlemanWunsch
 
 readLength :: Int
-readLength = 5
+readLength = 25
 
 
 --Make sequence data of length multiple of readLength
@@ -46,8 +46,7 @@ takeSeq i s = fromStr $ take i (toStr s)
 
 
 test1 :: Test
-test1 =  indexer refSeqShort ~?= [(fromStr "TTGGC", 2), (fromStr "TCAAG", 1), (fromStr "ACTGG", 0)]
-
+test1 =  indexer refSeq ~?= [(fromStr "TAGCTAGCTAGCTTCAGCTTTTTTT",3),(fromStr "AGTGATGGAAGACTCGAGGGTCTTT",2),(fromStr"GATCGATCGATCTTAAGGTGTGTAT",1),(fromStr "ACTGGTCAAGTTGGCCAATTGGCCA",0)]
 splitRef :: [(SeqData, Int)]
 splitRef = indexGenome refSeq [] 0
 
@@ -55,14 +54,46 @@ splitRef = indexGenome refSeq [] 0
 test2 :: Test
 test2 = align (toStr seq1) (toStr refSeqShort) ~?= ["AGCTG", "GTCGATGGATCGACTAGGCTAGCAT"]
 
---Call: alignment sequences (indexer refSeq)
---pairs each alignment with the corresponding genomic index
-alignment :: [SeqData] -> [(SeqData, Int)] -> [([String], Int)]
-alignment (x:xs) (y:ys) = [(align (toStr x) (toStr $ fst y), snd y)] ++ alignment xs (y:ys)
-alignment _ _           = [(["No sequences to align"], 0)]
+--Macth a read with it's bucket (returns similarity score of sequence with bucket)
+match_read :: SeqData -> [(SeqData, Int)] -> Int -> [(Int,Int)]
+--[([String], Int)]
+match_read s (y:ys) curr_bucket = (match (align (toStr s) (toStr $ fst y)) 0, curr_bucket) : (match_read s ys (curr_bucket+1)) where
+                                          match ((a:as):(b:bs):z) acc = 
+                                            if a == b then 
+                                              match (as:bs:z) (acc+1)
+                                            else 
+                                              match (as:bs:z) acc
+                                          match _ acc = acc
+match_read _ _ _ = []
 
+--find max of list of tuples based on first element
+max_fst :: Ord t => [(t, a)] -> (t, a)
+max_fst (x:xs) = maxT x xs
+  where maxT currMax [] = currMax
+        maxT (a, b) (p:ps)
+          | a < (fst p) = maxT p ps
+          | otherwise   = maxT (a, b) ps
+max_fst _     = error "no max on empty list"
+
+--  match reads with buckets (takes a list of reads and an indexed reference genome, 
+--	returns a list of reads with the bucket it matched to)
+match_reads :: [SeqData]-> [(SeqData, Int)] -> [(SeqData,Int)]
+match_reads (s:ss) r = (s, snd (max_fst (match_read s r 0))): (match_reads ss r)
+match_reads _ _ = []
+ 
+
+--needs to be implemented
 test3 :: Test
-test3 = alignment sequences (indexer refSeq) ~?= [(["AGCTG"], 15), (["AGCTG"], 13)]
+test3 = match_reads ref_sequences (indexer refSeq) ~?= []
+
+--given a list of reads, align each to reference
+align_reads :: [SeqData] -> SeqData -> [String]
+align_reads xs ref = filter (/= toStr ref) (redun xs ref) where
+                        redun (l:ls) r = align (toStr l) (toStr r) ++ redun ls r
+                        redun _ _      = []
+
+test_align_reads :: Test
+test_align_reads = align_reads ref_sequences refSeq ~?= []
 
 --pairs each alignment with the corresponding genomic index
 {-alignScores :: [SeqData] ->  [(SeqData, Int)] -> [([String], Int)]
--- TestData.hs
+++ TestData.hs
@@ -13,50 +13,53 @@ refSeq = fromStr "ACTGGTCAAGTTGGCCAATTGGCCAGATCGATCGATCTTAAGGTGTGTATAGTGATGGAAGA
 refSeqShort :: SeqData
 refSeqShort = fromStr "ACTGGTCAAGTTGGC"
 
---Individual DNA "reads"
-
+--Individual DNA "reads" no mutations
 seq1 :: SeqData
-seq1 = fromStr "ACTGG"
+seq1 = fromStr "ACTGGTCAAGTTGGC"
 seq2 :: SeqData
-seq2 = fromStr "GGCCATT"
+seq2 = fromStr "TGGCCAATTGGCCAGATC"
 seq3 :: SeqData
-seq3 = fromStr "GATCGAT"
+seq3 = fromStr "CAGATCGATCGATCTT"
 seq4 :: SeqData
-seq4 = fromStr "TCGATC"
+seq4 = fromStr "TCTTAAGGTGTGTATAGT"
 seq5 :: SeqData
-seq5 = fromStr "GCTAGCT"
+seq5 = fromStr "ATAGTGATGGAAGACTC"
 seq6 :: SeqData
-seq6 = fromStr "GAGGGT"
+seq6 = fromStr "GACTCGAGGGTCTTT"
 seq7 :: SeqData
-seq7 = fromStr "TAGCTAG"
+seq7 = fromStr "CTTTTAGCTAGCTAGCTTCAGCT"
 seq8 :: SeqData
-seq8 = fromStr "CTTTT"
+seq8 = fromStr "AGCTTCAGCT"
 seq9 :: SeqData
-seq9 = fromStr "TTGGCCAG"
+seq9 = fromStr "GGAAGACT"
 seq10 :: SeqData
-seq10 = fromStr "CAAGT"
+seq10 = fromStr "GATCGATCGAT"
+
+-- with mutations
 seq11 :: SeqData
-seq11 = fromStr "TCTTA"
+seq11 = fromStr "ACTGGTCACGTTGGC"
 seq12 :: SeqData
-seq12 = fromStr "CTCTA"
+seq12 = fromStr "TGGCCAATTGGCCAGATA"
 seq13 :: SeqData
-seq13 = fromStr "GTGA"
+seq13 = fromStr "CAGATCGATCGATCTT"
 seq14 :: SeqData
-seq14 = fromStr "TGATG"
+seq14 = fromStr "TCTTAAGGAGTGTATAGT"
 seq15 :: SeqData
-seq15 = fromStr "TGGAA"
+seq15 = fromStr "ATAGTGATGGAAGACTC"
 seq16 :: SeqData
-seq16 = fromStr "CCAAT"
+seq16 = fromStr "GACTCGAGGGTCTTA"
 seq17 :: SeqData
-seq17 = fromStr "GTCAAG"
+seq17 = fromStr "CTTTTAGCTAGCTAGCTTCAGCT"
 seq18 :: SeqData
-seq18 = fromStr "GTGTG"
+seq18 = fromStr "AGCTTCAGCA"
 seq19 :: SeqData
-seq19 = fromStr "CTCGA"
+seq19 = fromStr "GGAAGACT"
 seq20 :: SeqData
-seq20 = fromStr "TTCA"
+seq20 = fromStr "CATCGATCGAT"
 
 
-sequences :: [SeqData]
-sequences = [seq1, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq10, 
-             seq11, seq12, seq13, seq14, seq15, seq16, seq17, seq18, seq19, seq20]
+ref_sequences :: [SeqData]
+ref_sequences = [seq1, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq10]
+
+mut_sequences :: [SeqData]
+mut_sequences = [seq11, seq12, seq13, seq14, seq15, seq16, seq17, seq18, seq19, seq20]
